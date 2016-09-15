244960db45c0be3f9aa67de7554fbbba5846f936
Grey Delamar <Grey@Greys-MacBook-Air.local>
2016-09-15 12:29:17 -0600

    heroku test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -55,6 +56,44 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+const api = require('./api/index.js');
+
+app.use('/api', api)
+
+-- in api/index.js add the api data
+'use strict'
+
+const express = require('express');
+let router = express.Router();
+const pg = require('../db/knex_config.js');
+
+router.post('/v1/items',(req, res, next) => {
+  //console.log(pg)
+  pg('todos').insert(req.body)
+  .then(() =>{
+    res.redirect('/')
+  })
+  .catch((err)=>{
+    console.log('there was an error')
+    next(err)
+  })
+});
+
+router.get('/v1/items/delete/:id', (req, res, next) => {
+  // console.log("the id is: ", req.params.id);
+  // res.json(req.params)
+  pg('todos').where('id', req.params.id).del()
+  .then((something)=>{
+    console.log(something)
+  res.redirect('/')
+})
+  .catch((err) => {
+  console.error("error deleting from db");
+  next(err);
+  });
+});
+
+module.exports = router;
 
 
 module.exports = app;
