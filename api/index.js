712cdd986e3a87941056fd222192731b9b034f13
Grey Delamar <Grey@Greys-MacBook-Air.local>
2016-09-15 14:35:35 -0600

    test

--- api/index.js
+++ api/index.js
@@ -16,17 +16,38 @@ router.post('/v1/items',(req, res, next) => {
   })
 });
 
-router.put('/v1/items/edit/:id', function(req,res){
-  pg('post').where('id', req.params.id).update(req.body)
-  .then(() =>{
-    res.redirect('/')
-  })
-  .catch((err)=>{
-    console.log('there was an error')
-    next(err)
-  })
+router.get('/v1/items/edit/:id',(req, res, next) => {
+  res.render('/edit')
 });
 
+// router.put('/v1/items/edit/:id', function(req,res){
+//   pg('post').where('id', req.params.id).update(req.body)
+//   .then(() =>{
+//     res.redirect('/')
+//   })
+//   .catch((err)=>{
+//     console.log('there was an error')
+//     next(err)
+//   })
+// });
+// exports.edit = function(req, res){
+//
+//   var id = req.params.id;
+//
+//   req.getConnection(function(err,connection){
+//
+//      connection.query('SELECT * FROM customer WHERE id = ?',[id],function(err,rows)
+//         {
+//
+//             if(err)
+//                 console.log("Error Selecting : %s ",err );
+//
+//             res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
+//
+//          });
+//
+//     });
+// };
 
 router.get('/v1/items/delete/:id', (req, res, next) => {
   pg('post').where('id', req.params.id).del()
